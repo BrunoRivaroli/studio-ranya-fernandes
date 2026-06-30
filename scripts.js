dd39ee3227e1497a3e60f570a6f9a8a176e8239e
BrunoRivaroli <bruno.rivaroli26@gmail.com>
2026-06-30 14:19:16 -0300

    Inclusão do menu lateral

--- scripts.js
+++ scripts.js
@@ -1,1 +1,15 @@
-const 
+const menu = document.querySelector(".button-menu")
+const sideMenu = document.querySelector("ul")
+
+function showMenu() {
+  const isOpen = sideMenu.style.transform === "translateX(0%)";
+
+  if (isOpen) {
+    sideMenu.style.transform = "translateX(-100%)"
+  }
+  else { sideMenu.style.transform = "translateX(0%)" }
+}
+
+
+menu.addEventListener("click", showMenu)
+
